17870b8bc210bdad6ebaef32112fc19922b424dc
ANKITs-Projects <ankitgupta0429@gmail.com>
2026-04-23 11:32:01 +0530

    Db setup for vercel

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config({path: './.env'})
 
-require('./src/config/dbConfig')
+const connectDB = require('./src/config/dbConfig')
 
 
 const app = require("./src/app")
 
-const port  = process.env.PORT || 8000
+connectDB();
+
+// const port  = process.env.PORT || 8000
 
 // app.listen(port, () => {
 //     console.log(`Server is started at PORT: ${port}`)
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_URI)
+// mongoose.connect(process.env.DB_URI)
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('connected', () => {
-    console.log("DB Connection Successful")
-})
+// db.on('connected', () => {
+//     console.log("DB Connection Successful")
+// })
 
-db.on('error', (err) => {
-    console.log(`db connection failed : ${err.message}`)
-})
+// db.on('error', (err) => {
+//     console.log(`db connection failed : ${err.message}`)
+// })
+
+MONGO_URI = process.env.DB_URI
+
+let isConnected = false;
+
+const connectDB = async () => {
+    if (isConnected) return;
+
+    try {
+        const db = await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        isConnected = db.connections[0].readyState;
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("DB connection error:", error);
+        throw error;
+    }
+};
+
+module.exports = connectDB;
